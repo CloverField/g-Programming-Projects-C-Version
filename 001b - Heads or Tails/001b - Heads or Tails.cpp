3be12594cf80a38abbc1b9efe8b873ca535d741c
Clover <blazelion3@gmail.com>
2016-08-24 20:20:29 -0400

    Completed project 001b - Heads or Tails.

--- 001b - Heads or Tails/001b - Heads or Tails.cpp
+++ 001b - Heads or Tails/001b - Heads or Tails.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <string>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cout << "This program flips a coin and asks the user to guess it." << endl;
+	cout << "Enter your responce in the form of Heads Tails" << endl;
+	cout << "The program will keep running until you guess correctly." << endl;
+	bool running = true;
+	do
+	{
+		bool headsOrTails = rand() % 2;
+		string userGuess;
+		cout << "\nEnter your guess: ";
+		getline(cin, userGuess);
+		if ((userGuess == "Heads") && headsOrTails)
+		{
+			cout << "\nYou got it!\n";
+			running = false;
+		}
+		else if ((userGuess == "Tails") && !headsOrTails)
+		{
+			cout << "\nYou got it!\n";
+			running = false;
+		}
+		else
+		{
+			cout << "\nGuess again!\n";
+		}
+		
+	} while (running);
+	system("Pause");
+
 	return 0;
 }
-
