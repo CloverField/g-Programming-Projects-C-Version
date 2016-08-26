6c16e9d0243f22b2ef875702fe7d78ef71d7771c
Clover <blazelion3@gmail.com>
2016-08-26 17:47:01 -0400

    Completed project 005 - FizzBuzz

--- 005 - FizzBuzz/005 - FizzBuzz.cpp
+++ 005 - FizzBuzz/005 - FizzBuzz.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	for (int i = 1; i <= 100; i++)
+	{
+		if ((i%3 == 0) && (i%5 == 0))
+		{
+			cout << "Fizzbuzz\n";
+		}
+		else if (i%3 == 0)
+		{
+			cout << "Fizz\n";
+		}
+		else if (i % 5 == 0)
+		{
+			cout << "Buzz\n";
+		}
+		else
+		{
+			cout << i << "\n";
+		}
+	}
+
+	system("pause");
 	return 0;
 }
 
