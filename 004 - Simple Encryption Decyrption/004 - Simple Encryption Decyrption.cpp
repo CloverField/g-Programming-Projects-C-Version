dadaf702a9f37e375950175b9b7eeb96fe52f4c6
Clover <blazelion3@gmail.com>
2016-08-26 16:48:38 -0400

    Finished Project 004 - Simple Encryption Decryption

--- 004 - Simple Encryption Decyrption/004 - Simple Encryption Decyrption.cpp
+++ 004 - Simple Encryption Decyrption/004 - Simple Encryption Decyrption.cpp
@@ -2,10 +2,43 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
+string encypt(string s)
+{
+	string encyptedString;
+	for (int i = 0; i < s.length(); i++)
+	{
+		char c = s[i];
+		encyptedString += ((char)((int)c + 1));
+	}
+	return encyptedString;
+}
+
+string decrypt(string s)
+{
+	string decryptedString;
+	for (int i = 0; i < s.length(); i++)
+	{
+		char c = s[i];
+		decryptedString += ((char)((int)c - 1));
+	}
+	return decryptedString;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	string toBeEncrypted;
+	string encrypted;
+	cout << "This program performs simple ecryption and decryption on a user entered string." << endl;
+	cout << "Enter the string you want to encrypt: ";
+	cin >> toBeEncrypted;
+	encrypted = encypt(toBeEncrypted);
+	cout << "Your encrypted string is: " << encrypted << endl;
+	cout << "Your decrypted string is: " << decrypt(encrypted) << endl;
+	system("Pause");
 	return 0;
 }
 
